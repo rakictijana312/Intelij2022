7cd64cdd43f6faa713be33235f9349c80808aaea
sohel123 <sohel.abbasi@yash.com>
2021-06-06 00:28:26 +0530

    Class 3

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith;
         //unimplemented steps
 
         tags = "@example"
-        //tags will identify   the scenarios based on the tags we will provide such as @smoke, @regression etc
+        //tags will identify the scenarios based on the tags we will provide such as @smoke, @regression etc
         // we can add multiple tags in the runner class to execute scenarios belong to different tags such
         //as and or ,
 )
