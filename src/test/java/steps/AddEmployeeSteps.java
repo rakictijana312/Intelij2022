086b739bc673a577f0172b4febce4e2f69df73d8
Asghar Nazir <asghar@syntaxtech.com>
2021-07-03 14:38:56 -0400

    Database testing Added

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -4,6 +4,8 @@ import com.google.gson.stream.JsonToken;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.http.util.Asserts;
+import org.junit.Assert;
 import pages.AddEmployeePage;
 import pages.DashBoardPage;
 import utils.CommonMethods;
@@ -112,4 +114,22 @@ public class AddEmployeeSteps extends CommonMethods {
        AddEmployeePage addEmployeePage=new AddEmployeePage();
        GlobalVariables.empId=addEmployeePage.employeeId.getAttribute("value");
     }
+
+    @Then("verify the data from frontend and backend")
+    public void verify_the_data_from_frontend_and_backend() {
+        System.out.println("Backend");
+        System.out.println("DBFirstName"+GlobalVariables.dbFirstName);
+        System.out.println("DBMiddleName"+GlobalVariables.dbMiddleName);
+        System.out.println("DBLastName"+GlobalVariables.dbLastName);
+        System.out.println("FrontEnd");
+        System.out.println("Firstname"+GlobalVariables.firstName);
+        System.out.println("MiddleName"+GlobalVariables.middleName);
+        System.out.println("Lastname"+GlobalVariables.lastName);
+        Assert.assertEquals(GlobalVariables.firstName,GlobalVariables.dbFirstName);
+        Assert.assertEquals(GlobalVariables.middleName,GlobalVariables.dbMiddleName);
+        Assert.assertEquals(GlobalVariables.lastName,GlobalVariables.dbLastName);
+
+    }
+
+
 }
