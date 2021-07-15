82902fc86db5ea2de7856b242df8f1970334da34
iamdez99 <diegojuarezbusiness@gmail.com>
2021-07-15 19:57:35 -0400

    Pushing changes

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -20,19 +20,39 @@ public class HardCodedExamples {
 	 * Then - verification/assertions
 	 */
 
-	@Test
+	String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+	String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjYzOTE5NjcsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyNjQzNTE2NywidXNlcklkIjoiMjk2MSJ9.u2W0fDhjhJZij5IXQZ9MoDli2_knXZF5vy5nZv8k2xA";
+
+	// @Test
 	public void sampleTest() {
 
-		String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
-
-		RequestSpecification preparedRequest = given().header("Authorization",
-				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjYzMDU0ODYsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyNjM0ODY4NiwidXNlcklkIjoiMjk2MSJ9.j8ws9_rIGldrI9Urzr_gzCM4rIKQ5hAWU8pbrGoLi0U")
+		RequestSpecification preparedRequest = given().header("Authorization", token)
 				.header("Content-Type", "application/json").queryParam("employee_id", "20333320");
 
 		Response response = preparedRequest.when().get("/getOneEmployee.php");
-
 		System.out.println(response.asString());
+	}
 
+	@Test
+	public void postCreateEmployee() {
+
+		RequestSpecification preparedRequest = given().header("Authorization", token).header("Content-Type", "application/json")
+						.body("{\n"
+								+ "  \"emp_firstname\": \"Syntax\",\n"
+								+ "  \"emp_lastname\": \"API\",\n"
+								+ "  \"emp_middle_name\": \"Instructor\",\n"
+								+ "  \"emp_gender\": \"F\",\n"
+								+ "  \"emp_birthday\": \"1990-07-10\",\n"
+								+ "  \"emp_status\": \"Employee\",\n"
+								+ "  \"emp_job_title\": \"Healer\"\n"
+								+ "}").log().all();
+		
+		Response response = preparedRequest.when().post("/createEmployee.php");
+		
+		response.prettyPrint();
+		
+		
+		
 	}
 
 }
