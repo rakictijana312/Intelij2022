c5a64dcc997a387203d7006639cd89eecd9c622d
iamdez99 <diegojuarezbusiness@gmail.com>
2021-07-15 20:45:57 -0400

    Pushing changes

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -23,7 +23,8 @@ public class HardCodedExamples {
 
 	String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
 	String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjYzOTE5NjcsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyNjQzNTE2NywidXNlcklkIjoiMjk2MSJ9.u2W0fDhjhJZij5IXQZ9MoDli2_knXZF5vy5nZv8k2xA";
-
+	static String employee_id;
+	
 	// @Test
 	public void sampleTest() {
 
@@ -31,9 +32,10 @@ public class HardCodedExamples {
 				.header("Content-Type", "application/json").queryParam("employee_id", "20333320");
 
 		Response response = preparedRequest.when().get("/getOneEmployee.php");
-		
+
 		/*
-		 * Printing response using aString() method to convert JSON object to a String and printing using System.Out.Println()
+		 * Printing response using aString() method to convert JSON object to a String
+		 * and printing using System.Out.Println()
 		 */
 		System.out.println(response.asString());
 	}
@@ -41,55 +43,62 @@ public class HardCodedExamples {
 	@Test
 	public void postCreateEmployee() {
 
-		RequestSpecification preparedRequest = given().header("Authorization", token).header("Content-Type", "application/json")
-						.body("{\n"
-								+ "  \"emp_firstname\": \"Syntax\",\n"
-								+ "  \"emp_lastname\": \"API\",\n"
-								+ "  \"emp_middle_name\": \"Instructor\",\n"
-								+ "  \"emp_gender\": \"F\",\n"
-								+ "  \"emp_birthday\": \"1990-07-10\",\n"
-								+ "  \"emp_status\": \"Employee\",\n"
-								+ "  \"emp_job_title\": \"Healer\"\n"
-								+ "}").log().all();
-		
+		RequestSpecification preparedRequest = given().header("Authorization", token)
+				.header("Content-Type", "application/json")
+				.body("{\n" + "  \"emp_firstname\": \"Syntax\",\n" + "  \"emp_lastname\": \"API\",\n"
+						+ "  \"emp_middle_name\": \"Instructor\",\n" + "  \"emp_gender\": \"F\",\n"
+						+ "  \"emp_birthday\": \"1990-07-10\",\n" + "  \"emp_status\": \"Employee\",\n"
+						+ "  \"emp_job_title\": \"Healer\"\n" + "}")
+				.log().all();
+
 		/*
 		 * 
-		 * log().all() will log and print all information being sent with the request 
+		 * log().all() will log and print all information being sent with the request
 		 */
-		
+
 		Response response = preparedRequest.when().post("/createEmployee.php");
-		
+
 		/**
-		 * prettyPrint() does the same as System.out.println(response.asString()); 
+		 * prettyPrint() does the same as System.out.println(response.asString());
 		 */
 		response.prettyPrint();
-		
+
 		/**
-		 * jsonPath() allows us to retrieve specific data from a json object - just like an xpath with selenium
+		 * jsonPath() allows us to retrieve specific data from a json object - just like
+		 * an xpath with selenium
 		 */
-		String employee_id = response.jsonPath().getString("Employee.employee_id");
-		
+		employee_id = response.jsonPath().getString("Employee.employee_id");
+
 		System.out.println(employee_id);
-		
+
 		/**
 		 * Performing assertions
 		 */
 		response.then().assertThat().statusCode(201);
-		
+
 		/**
 		 * Using Hamcrest Matchers class equalTo()
 		 */
 		response.then().assertThat().body("Message", equalTo("Employee Created"));
-		
-		// Write an assertion that verifies that the response body has the name you used 
-		
+
+		// Write an assertion that verifies that the response body has the name you used
+
 		response.then().assertThat().body("Employee.emp_firstname", equalTo("Syntax"));
-		
+
 		/**
 		 * Verifying server
 		 */
 		response.then().assertThat().header("Server", "Apache/2.4.39 (Win64) PHP/7.2.18");
 	}
-	
-	
+
+	@Test
+	public void getCreatedEmployee() {
+
+		RequestSpecification preparedRequest = given().header("Authorization", token).header("Content-Type", "application/json").queryParam("employee_id", employee_id);
+		
+		Response response = preparedRequest.when().get("/getOneEmployee.php");
+		
+		response.prettyPrint();
+
+	}
 }
