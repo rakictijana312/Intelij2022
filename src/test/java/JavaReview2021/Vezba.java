d9e1aa4793bbe6f06be8587ecfbfa998767b8e55
rakic <rakicijana312@gmail.com>
2022-09-07 15:18:14 +0200

    commit vezba

--- src/test/java/JavaReview2021/Vezba.java
+++ src/test/java/JavaReview2021/Vezba.java
@@ -3,6 +3,13 @@ package JavaReview2021;
 import org.apache.poi.util.SystemOutLogger;
 
 public class Vezba {
-    //sfrsrvrvrsfdrdfdfvdfvdfvdfdf
-   //efwefwef
+    public static void main(String[] args) {
+
+
+        //sfrsrvrvrsfdrdfdfvdfvdfvdfdf
+        //efwefwef
+        String a = "tijana";
+        System.out.println(a);
+
+    }
 }
