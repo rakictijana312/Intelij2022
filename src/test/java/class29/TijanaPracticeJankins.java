53a0acd1d5b6d20f3b28f1ac1b650ce70dd242b0
rakic <rakicijana312@gmail.com>
2023-06-16 11:45:35 -0400

    updated code 06 - 16

---
+++ src/test/java/class29/TijanaPracticeJankins.java
@@ -0,0 +1,20 @@
+package class29;
+
+public class TijanaPracticeJankins {
+    public static void main(String[] args) {
+        int given = 10;
+        boolean isPrime=true;
+        if(given>1) {
+            for(int i=2; i<given; i++) {
+                if(given%i==0) {
+                    isPrime=false;
+                    break;
+                }
+            }
+
+        }else {
+            isPrime=false;
+        }
+        System.out.println("Given number "+given+" is prime? "+isPrime);
+    }
+}
